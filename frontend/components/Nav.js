536581ae90eba9d3501671160b311f2187114a23
Mathew Williamson <mwilliamson@64labs.com>
2019-03-20 08:29:16 -0400

    First commit of the basics

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -1,27 +1,27 @@
-import React, { Component } from 'react'
-import Link from 'next/link'
-import NavStyles from './styles/NavStyles'
+import React, { Component } from "react";
+import Link from "next/link";
+import NavStyles from "./styles/NavStyles";
 
 const Nav = () => {
-    return (
-        <NavStyles>
-            <Link href='/items'>
-                <a>Shop</a>
-            </Link>
-            <Link href='/sell'>
-                <a>Sell</a>
-            </Link>    
-            <Link href='/signup'>
-                <a>Signup</a>
-            </Link>
-            <Link href='/orders'>
-                <a>Orders</a>
-            </Link>  
-            <Link href='/me'>
-                <a>Account</a>
-            </Link>  
-        </NavStyles>    
-    )
-}
- 
+  return (
+    <NavStyles>
+      <Link href="/items">
+        <a>Test</a>
+      </Link>
+      <Link href="/sell">
+        <a>Sell</a>
+      </Link>
+      <Link href="/signup">
+        <a>Signup</a>
+      </Link>
+      <Link href="/orders">
+        <a>Orders</a>
+      </Link>
+      <Link href="/me">
+        <a>Account</a>
+      </Link>
+    </NavStyles>
+  );
+};
+
 export default Nav;
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,10 +1,12 @@
 import React from "react";
-import Items from "../components/Items";
+// import Items from "../components/Items";
 
+// [matt]: ADD TO THIS
 const Home = props => {
   return (
     <div>
-      <Items page={parseFloat(props.query.page) || 1} />
+      {/* <Items page={parseFloat(props.query.page) || 1} /> */}
+      <p>This is the main page</p>
     </div>
   );
 };
