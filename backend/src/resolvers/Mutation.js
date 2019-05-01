c1f5e1447d356396ebc8cc71926ac3c31331514d
Mathew Williamson <mwilliamson@64labs.com>
2019-05-01 08:39:50 -0400

    Started on Reset Password Functionality

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,5 +1,7 @@
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+const { randomBytes } = require('crypto')
+const { promisify } = require('util')
 
 const Mutation = {
     // User Mutations
@@ -52,6 +54,26 @@ const Mutation = {
     signout(parent, args, ctx, info) {
         ctx.response.clearCookie('token')
         return { message: 'Goodbye!'}
+    },
+
+    async requestReset(parent, args, ctx, info) {
+        // 1. Check if this is a real user
+        const user = await ctx.db.query.user({ where: { email: args.email}})
+        if (!user) {
+            throw new Error(`No such user found for email ${args.email}`)
+        }
+        // 2. Set a reset token and expiry on that User
+        const randomBytesPromisified = promisify(randomBytes)
+        const resetToken = (await randomBytesPromisified(20)).toString('hex')
+        const resetTokenExpiry = Date.now() + 3600000 // 1 hour from now
+        const res = await ctx.db.mutation.updateUser({
+            where: { email: args.email },
+            data: { resetToken, resetTokenExpiry}
+        })
+        console.log('[matt] response', res)
+        return { message: 'Thanks!' }
+        
+        // 3. Email them that reset token
     },
     
     // Lead Mutations
